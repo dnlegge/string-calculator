c31771ddc607be5c7290248c09f4e946280a38c0
dnlegge <david@dnlegge.com>
2015-10-28 15:23:55 +0000

    Refactor

--- src/main/java/com/dnlegge/DavidsStringCalculator.java
+++ src/main/java/com/dnlegge/DavidsStringCalculator.java
@@ -2,6 +2,8 @@ package com.dnlegge;
 
 public class DavidsStringCalculator implements StringCalculator {
 
+    public static final String LIST_OF_TOKENS_TO_SPLIT_ON_REGEX = "[,\\n]";
+
     @Override
     public int add(String numbers) {
         //shortcut if empty - doesn't currently handle null gracefully
@@ -13,7 +15,7 @@ public class DavidsStringCalculator implements StringCalculator {
     }
 
     private int handleArbitraryListOfNumbers(String numbers) {
-        final String[] split = numbers.split("[,\\n]");
+        final String[] split = numbers.split(LIST_OF_TOKENS_TO_SPLIT_ON_REGEX);
 
         return iterateThroughArrayOfNumbers(split);
     }
