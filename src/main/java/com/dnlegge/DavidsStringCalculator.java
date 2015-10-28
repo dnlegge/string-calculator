369fd20a4d8cd73b1c8946ed1c19ad7ee4a6925d
dnlegge <david@dnlegge.com>
2015-10-28 14:58:14 +0000

    Passing tests

--- src/main/java/com/dnlegge/DavidsStringCalculator.java
+++ src/main/java/com/dnlegge/DavidsStringCalculator.java
@@ -4,6 +4,9 @@ public class DavidsStringCalculator implements StringCalculator {
 
     @Override
     public int add(String numbers) {
+        if (!numbers.isEmpty()) {
+            return Integer.parseInt(numbers);
+        }
         return 0;
     }
 }
