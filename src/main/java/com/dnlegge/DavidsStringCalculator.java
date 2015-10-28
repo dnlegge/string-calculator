81dbc7b92fe8a54980b157fdc8e1a19a95e52cdd
dnlegge <david@dnlegge.com>
2015-10-28 15:11:52 +0000

    Refactor

--- src/main/java/com/dnlegge/DavidsStringCalculator.java
+++ src/main/java/com/dnlegge/DavidsStringCalculator.java
@@ -4,18 +4,37 @@ public class DavidsStringCalculator implements StringCalculator {
 
     @Override
     public int add(String numbers) {
-        if (!numbers.isEmpty()) {
-            final String[] split = numbers.split(",");
-            int total = 0;
-            for (String number : split) {
-                total += parseNumbers(number.trim());
-            }
-            return total;
+        //shortcut if empty - doesn't currently handle null gracefully
+        if (numbers.isEmpty()) {
+            return 0;
         }
-        return 0;
+
+        return handleArbitraryListOfNumbers(numbers);
     }
 
-    private int parseNumbers(String numbers) {
-        return Integer.parseInt(numbers);
+    private int handleArbitraryListOfNumbers(String numbers) {
+        final String[] split = numbers.split(",");
+
+        return iterateThroughArrayOfNumbers(split);
+    }
+
+    private int iterateThroughArrayOfNumbers(String[] split) {
+        int total = 0;
+        for (String number : split) {
+            total += extractIntValue(number);
+        }
+        return total;
+    }
+
+    private int extractIntValue(String number) {
+        return parseNumber(sanitiseInput(number));
+    }
+
+    private String sanitiseInput(String number) {
+        return number.trim();
+    }
+
+    private int parseNumber(String number) {
+        return Integer.parseInt(number);
     }
 }
