2fcfb969beea4aab7a2936f09d4e28df9f37db62
dnlegge <david@dnlegge.com>
2015-11-02 09:16:01 +0000

    Confirmation that solution works with multiple arbitrary char delimiters

--- src/test/java/com/dnlegge/StringCalculatorTest.java
+++ src/test/java/com/dnlegge/StringCalculatorTest.java
@@ -146,4 +146,12 @@ public class StringCalculatorTest {
         assertEquals(6, add);
     }
 
+    @Test
+    public void addWithMultipleArbitraryCharDelimiterTest() {
+
+        final int add = davidsStringCalculator.add("//[**][%%]\n1**2%%3");
+
+        assertEquals(6, add);
+    }
+
 }
